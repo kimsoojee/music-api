e2a6dd9258885505bc116bb2096cf6f468649bbd
김수지 <ksssuzie@gmail.com>
2025-06-24 17:05:22 +0900

    Song 에 좋아요 수를 더하는 쿼리 추가

--- src/main/java/com/example/music/repository/SongRepository.java
+++ src/main/java/com/example/music/repository/SongRepository.java
@@ -1,8 +1,13 @@
 package com.example.music.repository;
 
 import com.example.music.model.Song;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import org.springframework.data.repository.query.Param;
+import reactor.core.publisher.Mono;
 
 public interface SongRepository extends R2dbcRepository<Song, Long> {
 
+  @Query("UPDATE songs SET likes = likes + 1 WHERE id = :songId")
+  Mono<Void> incrementLikes(@Param("songId") Long songId);
 }
