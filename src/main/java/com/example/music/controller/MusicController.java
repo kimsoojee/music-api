4d93662c1e465d4c726c1104839e54919f2ed0df
김수지 <ksssuzie@gmail.com>
2025-06-24 16:19:44 +0900

    앨범 수 조회 api 추가

---
+++ src/main/java/com/example/music/controller/MusicController.java
@@ -0,0 +1,25 @@
+package com.example.music.controller;
+
+import com.example.music.repository.projection.AlbumCount;
+import com.example.music.service.MusicService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequestMapping("/api/music")
+@RequiredArgsConstructor
+public class MusicController {
+
+  private final MusicService musicService;
+
+  @GetMapping("/statistics/albums/by-year-and-artist")
+  public Flux<AlbumCount> getAlbumCountByYearAndArtist(
+    @RequestParam(defaultValue = "0") int page,
+    @RequestParam(defaultValue = "10") int size) {
+    return musicService.getAlbumCountByYearAndArtist(page, size);
+  }
+}
