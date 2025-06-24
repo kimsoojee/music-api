938827c1bd83b12bcf9949d5c9901d492ab4e203
김수지 <ksssuzie@gmail.com>
2025-06-25 00:03:15 +0900

    테스트 데이터 생성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,30 @@
+DELETE FROM song_likes;
+DELETE FROM songs;
+DELETE FROM albums;
+
+ALTER TABLE albums ALTER COLUMN id RESTART WITH 1;
+ALTER TABLE songs ALTER COLUMN id RESTART WITH 1;
+ALTER TABLE song_likes ALTER COLUMN id RESTART WITH 1;
+
+INSERT INTO albums (title, artist, release_date, created_at, updated_at)
+VALUES
+    ('Popular Album', 'Popular Artist', '15/03/2024', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    ('Another Album', 'Another Artist', '20/06/2024', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    ('Third Album', 'Third Artist', '10/09/2024', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO songs (album_id, song, length, likes, created_at, updated_at)
+VALUES
+    (1, 'Most Liked Song', '3:30', 3, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (2, 'Second Most Liked', '4:00', 2, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    (3, 'Less Liked Song', '2:45', 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO song_likes (song_id, liked_at)
+VALUES
+    (1, DATEADD('MINUTE', -30, CURRENT_TIMESTAMP)),
+    (1, DATEADD('MINUTE', -25, CURRENT_TIMESTAMP)),
+    (1, DATEADD('MINUTE', -20, CURRENT_TIMESTAMP)),
+
+    (2, DATEADD('MINUTE', -15, CURRENT_TIMESTAMP)),
+    (2, DATEADD('MINUTE', -10, CURRENT_TIMESTAMP)),
+
+    (3, DATEADD('MINUTE', -5, CURRENT_TIMESTAMP));
