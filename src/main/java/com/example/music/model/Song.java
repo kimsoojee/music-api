433c28acf51f91ec2c1305a0d5d10af08459fc5f
김수지 <ksssuzie@gmail.com>
2025-06-24 15:49:21 +0900

    song 앤티티 생성

---
+++ src/main/java/com/example/music/model/Song.java
@@ -0,0 +1,68 @@
+package com.example.music.model;
+
+import java.time.LocalDateTime;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Data
+@Table("songs")
+@NoArgsConstructor
+public class Song {
+  @Id
+  private Long id;
+
+  private Long albumId;
+
+  private String song;
+  private String text;
+  private String length;
+  private String emotion;
+  private String genre;
+
+  private String musicalKey;
+  private Double tempo;
+  private Double loudness;
+
+  private String timeSignature;
+  private Boolean explicit = false;
+  private Integer popularity = 0;
+  private Integer energy;
+  private Integer danceability;
+  private Integer positiveness;
+  private Integer speechiness;
+  private Integer liveness;
+  private Integer acousticness;
+  private Integer instrumentalness;
+
+  private Long likes = 0L;
+
+  private Boolean goodForParty = false;
+  private Boolean goodForWorkStudy = false;
+  private Boolean goodForRelaxationMeditation = false;
+  private Boolean goodForExercise = false;
+  private Boolean goodForRunning = false;
+  private Boolean goodForYogaStretching = false;
+  private Boolean goodForDriving = false;
+  private Boolean goodForSocialGatherings = false;
+  private Boolean goodForMorningRoutine = false;
+
+  private String similarArtist1;
+  private String similarSong1;
+  private Double similarityScore1;
+  private String similarArtist2;
+  private String similarSong2;
+  private Double similarityScore2;
+  private String similarArtist3;
+  private String similarSong3;
+  private Double similarityScore3;
+
+  @CreatedDate
+  private LocalDateTime createdAt;
+
+  @LastModifiedDate
+  private LocalDateTime updatedAt;
+}
